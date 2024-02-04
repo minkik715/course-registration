8946438795edd40ee978155d0375d1da15e1e007
rlaalsrl715 <minkik715@gmail.com>
2024-02-04 17:41:54 +0900

    init

---
+++ src/test/kotlin/com/minkik715/coderegistration/CodeRegistrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minkik715.coderegistration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeRegistrationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
