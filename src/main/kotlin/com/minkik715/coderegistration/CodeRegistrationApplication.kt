4c001ee645cc5814b1d65d485076dd0de51d635e
rlaalsrl715 <minkik715@gmail.com>
2024-02-04 17:41:54 +0900

    init

---
+++ src/main/kotlin/com/minkik715/coderegistration/CodeRegistrationApplication.kt
@@ -0,0 +1,11 @@
+package com.minkik715.coderegistration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeRegistrationApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeRegistrationApplication>(*args)
+}
